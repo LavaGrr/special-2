a60c1061ec0e4b9010f9551c78fb379434bc4e4d
LavaGrr <luizavianabh@gmail.com>
2019-08-19 22:39:19 -0300

    app_1.0
    primeira versao (apenas com um component para a sessao de dados da colmeia)
    NAO SEI USAR O NEGOCIO DE STYLES

--- appMeta/index.js
+++ appMeta/index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './src';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+//https://pt.stackoverflow.com/questions/203299/como-executar-o-emulador-a-partir-da-linha-de-comandos-do-windows
---
+++ appMeta/src/components/dadosColmeia/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native' ;
+import DadosColmeia from '.';
+
+const styles = StyleSheet.create({
+    container:{
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+    },
+    dadosColmeia:{
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+    }
+})
